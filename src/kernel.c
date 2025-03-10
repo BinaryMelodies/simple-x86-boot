bd33f85d51ba0f9aa0bc1bcc7c6d5c8396f2eebd
Binary Melodies <192211378+BinaryMelodies@users.noreply.github.com>
2025-03-10 19:54:06 +0100

    Rename identifiers from video_ to screen_

--- src/kernel.c
+++ src/kernel.c
@@ -19,109 +19,109 @@ const char greeting[] = "Greetings! OS/64 running in 64-bit long mode";
 
 enum
 {
-	VIDEO_WIDTH = 80,
-	VIDEO_HEIGHT = 25
+	SCREEN_WIDTH = 80,
+	SCREEN_HEIGHT = 25
 };
 
-static uint8_t video_x = 0, video_y = 0, video_attribute = 0x07;
+static uint8_t screen_x = 0, screen_y = 0, screen_attribute = 0x07;
 
 #if OS86
-uint16_t far * const video_buffer = (uint16_t far *)MK_FP(0xB800, 0);
+uint16_t far * const screen_buffer = (uint16_t far *)MK_FP(0xB800, 0);
 #elif OS286
-uint16_t far * const video_buffer = (uint16_t far *)MK_FP(0x0018, 0);
+uint16_t far * const screen_buffer = (uint16_t far *)MK_FP(0x0018, 0);
 #else
-uint16_t * const video_buffer = (uint16_t *)0x000B8000;
+uint16_t * const screen_buffer = (uint16_t *)0x000B8000;
 #endif
 
-static inline void video_set_word(int offset, uint16_t value)
+static inline void screen_set_word(int offset, uint16_t value)
 {
-	video_buffer[offset] = value;
+	screen_buffer[offset] = value;
 }
 
-static inline uint16_t video_get_word(int offset)
+static inline uint16_t screen_get_word(int offset)
 {
-	return video_buffer[offset];
+	return screen_buffer[offset];
 }
 
-static inline void video_scroll_lines(int count)
+static inline void screen_scroll_lines(int count)
 {
-	if(count > VIDEO_HEIGHT)
+	if(count > SCREEN_HEIGHT)
 	{
-		count = VIDEO_HEIGHT;
+		count = SCREEN_HEIGHT;
 	}
-	for(int i = 0; i < VIDEO_WIDTH * (VIDEO_HEIGHT - count); i++)
+	for(int i = 0; i < SCREEN_WIDTH * (SCREEN_HEIGHT - count); i++)
 	{
-		video_buffer[i] = video_buffer[i + VIDEO_WIDTH * count];
+		screen_buffer[i] = screen_buffer[i + SCREEN_WIDTH * count];
 	}
-	for(int i = 0; i < VIDEO_WIDTH * count; i++)
+	for(int i = 0; i < SCREEN_WIDTH * count; i++)
 	{
-		video_buffer[i + VIDEO_WIDTH * (VIDEO_HEIGHT - count)] = (video_attribute << 8) | ' ';
+		screen_buffer[i + SCREEN_WIDTH * (SCREEN_HEIGHT - count)] = (screen_attribute << 8) | ' ';
 	}
 }
 
-static inline void video_move_cursor(void)
+static inline void screen_move_cursor(void)
 {
-	uint16_t location = video_y * VIDEO_WIDTH + video_x;
+	uint16_t location = screen_y * SCREEN_WIDTH + screen_x;
 	outp(0x3D4, 0x0E);
 	outp(0x3D5, location >> 8);
 	outp(0x3D4, 0x0F);
 	outp(0x3D5, location);
 }
 
-static inline void video_putchar(int c)
+static inline void screen_putchar(int c)
 {
 	switch(c)
 	{
 	case '\b':
-		if(video_x > 0)
+		if(screen_x > 0)
 		{
-			video_x--;
+			screen_x--;
 		}
 		break;
 	case '\t':
-		video_x = (video_x + 8) & ~7;
+		screen_x = (screen_x + 8) & ~7;
 		break;
 	case '\n':
-		video_x = 0;
-		video_y++;
+		screen_x = 0;
+		screen_y++;
 		break;
 	default:
 		if(' ' <= c && c <= '~')
 		{
-			video_set_word(video_y * VIDEO_WIDTH + video_x, (video_attribute << 8) + (uint8_t)c);
-			video_x++;
+			screen_set_word(screen_y * SCREEN_WIDTH + screen_x, (screen_attribute << 8) + (uint8_t)c);
+			screen_x++;
 		}
 		break;
 	}
-	if(video_x >= VIDEO_WIDTH)
+	if(screen_x >= SCREEN_WIDTH)
 	{
-		video_y += video_x / VIDEO_WIDTH;
-		video_x %= VIDEO_WIDTH;
+		screen_y += screen_x / SCREEN_WIDTH;
+		screen_x %= SCREEN_WIDTH;
 	}
-	if(video_y >= VIDEO_HEIGHT)
+	if(screen_y >= SCREEN_HEIGHT)
 	{
-		video_scroll_lines(video_y + 1 - VIDEO_HEIGHT);
-		video_y = VIDEO_HEIGHT - 1;
+		screen_scroll_lines(screen_y + 1 - SCREEN_HEIGHT);
+		screen_y = SCREEN_HEIGHT - 1;
 	}
-	video_move_cursor();
+	screen_move_cursor();
 }
 
-static inline void video_putstr(const char far * text)
+static inline void screen_putstr(const char far * text)
 {
 	for(int i = 0; text[i] != '\0'; i++)
 	{
-		video_putchar((uint8_t)text[i]);
+		screen_putchar((uint8_t)text[i]);
 	}
 }
 
 noreturn void kmain(void)
 {
-	video_attribute = 0x1E;
-	video_putstr(greeting);
+	screen_attribute = 0x1E;
+	screen_putstr(greeting);
 
-	for(int i = 0; i < VIDEO_HEIGHT; i++)
+	for(int i = 0; i < SCREEN_HEIGHT; i++)
 	{
-		video_putstr("scroll test\n");
+		screen_putstr("scroll test\n");
 	}
 
 	for(;;)
